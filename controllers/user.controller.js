28d844d13783553a94967bdda6e3d97b916cc770
Louis <louislebail@protonmail.com>
2022-08-31 19:28:29 +0200

    CRUD : follow/unfollow

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -47,14 +47,81 @@ module.exports.updateUser = async (req, res) => {
   }
 };
 
-module.exports.deleteUser = async (req, res) => {
+module.exports.deleteUser = (req, res) => {
   if (!ObjectID.isValid(req.params.id))
     return res.status(400).send("ID unknown : " + req.params.id);
 
   try {
-    await UserModel.remove({ _id: req.params.id }).exec();
+     UserModel.remove({ _id: req.params.id }).exec();
     res.status(200).json({ message: "Successfully deleted. " });
   } catch (err) {
     return res.status(500).json({ message: err });
   }
 };
+
+module.exports.follow = (req, res) => {
+    if (
+      !ObjectID.isValid(req.params.id) ||
+      !ObjectID.isValid(req.body.idToFollow)
+    )
+      return res.status(400).send("ID unknown : " + req.params.id);
+  
+    try {
+      // add to the follower list
+       UserModel.findByIdAndUpdate(
+        req.params.id,
+        // ajouter au tableau de personnes suivies par le user
+        { $addToSet: { following: req.body.idToFollow } },
+        { new: true, upsert: true },
+        (err, docs) => {
+          if (!err) res.status(201).json(docs);
+          else return res.status(400).json(err);
+        }
+      );
+      // add to following list
+       UserModel.findByIdAndUpdate(
+        req.body.idToFollow,
+        { $addToSet: { followers: req.params.id } },
+        { new: true, upsert: true },
+        (err, docs) => {
+          // if (!err) res.status(201).json(docs);
+          if (err) return res.status(400).json(err);
+        }
+      );
+    } catch (err) {
+      return res.status(500).json({ message: err });
+    }
+  };
+  
+  module.exports.unfollow = (req, res) => {
+    if (
+      !ObjectID.isValid(req.params.id) ||
+      !ObjectID.isValid(req.body.idToUnfollow)
+    )
+      return res.status(400).send("ID unknown : " + req.params.id);
+  
+    try {
+       UserModel.findByIdAndUpdate(
+        req.params.id,
+        { $pull: { following: req.body.idToUnfollow } },
+        { new: true, upsert: true },
+        (err, docs) => {
+          if (!err) res.status(201).json(docs);
+          else return res.status(400).json(err);
+        }
+      );
+      // remove to following list
+       UserModel.findByIdAndUpdate(
+        req.body.idToUnfollow,
+        { $pull: { followers: req.params.id } },
+        { new: true, upsert: true },
+        (err, docs) => {
+          // if (!err) res.status(201).json(docs);
+          if (err) return res.status(400).json(err);
+        }
+      );
+    } catch (err) {
+      return res.status(500).json({ message: err });
+    }
+  };
+  
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -10,5 +10,8 @@ router.get('/', userController.getAllUsers);
 router.get('/:id', userController.userInfo);
 router.put('/:id', userController.updateUser);
 router.delete("/:id", userController.deleteUser);
+// patch : mettre à jour le tableau à l'interieur d'un user
+router.patch('/follow/:id', userController.follow);
+router.patch('/unfollow/:id', userController.unfollow);
 
 module.exports = router;
