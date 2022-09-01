811dbdef73756ce40892cbb32dc4d0a9e7a03a85
Louis <louislebail@protonmail.com>
2022-09-01 15:37:14 +0200

    Comment : routes, controller, post, edit, delete

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -9,4 +9,8 @@ router.delete('/:id', postController.deletePost);
 router.patch('/like-post/:id', postController.likePost);
 router.patch('/unlike-post/:id', postController.unlikePost);
 
+router.patch('/comment-post/:id', postController.commentPost);
+router.patch('/edit-comment-post/:id', postController.editCommentPost);
+router.patch('/delete-comment-post/:id', postController.deleteCommentPost);
+
 module.exports = router;
