1e4a6f90246c76d57a19eda21f19dc1128cefcf9
Louis <louislebail@protonmail.com>
2022-09-01 21:35:17 +0200

    upload/update profil image : route, controller, multer, errors,

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const authController = require ("../controllers/auth.controller");
 const userController = require ('../controllers/user.controller');
+const uploadController = require('../controllers/upload.controller');
+const multer = require("multer");
+const upload = multer();
 
 //auth
 router.post("/register", authController.signUp);
@@ -17,4 +20,6 @@ router.delete("/:id", userController.deleteUser);
 router.patch('/follow/:id', userController.follow);
 router.patch('/unfollow/:id', userController.unfollow);
 
+// upload
+router.post('/upload', upload.single('file'), uploadController.uploadProfil);
 module.exports = router;
